c6bee65f54ba66351036edce5f18bf14fcc8cd19
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2021-08-17 16:57:50 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,10 +14,14 @@ def hello_world():
     else:
         myage = request.form['age']
         mygender = request.form['gender']
-        model = load('app/bread-recommender.joblib')
-        np_arr = np.array([myage, mygender])
-        predictions = model.predict([np_arr])  
-        predictions_to_str = str(predictions)
-        #return predictions_to_str
-        return render_template('index.html', href2='The suitable bread for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
+        if str(myage) =='' or str(mygender) =='':
+            return render_template('index.html', href2='Please insert your age and gender.')
+        else:
+            model = load('app/bread-recommender.joblib')
+            np_arr = np.array([myage, mygender])
+            predictions = model.predict([np_arr])  
+            predictions_to_str = str(predictions)
+            #return predictions_to_str
+            return render_template('index.html', href2='The suitable bread for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
+        
 
